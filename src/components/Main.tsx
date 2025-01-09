e1648f5cbe9aacea9f8daf6e49eb28ff041a8956
Rahul Ranjan <rahulranjan@Rahuls-MacBook-Air.local>
2025-01-09 17:06:22 +0530

    profile picture fix

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,7 +9,7 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="/react-portfolio-template/profile.jpg" alt="Avatar" />
+          <img src="/rahul-ranjan/profile.jpg" alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
